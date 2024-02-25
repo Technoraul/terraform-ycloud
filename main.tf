7941d01d165dfa7554f271033f014f593a641c8f
Paul <p.infomail8@gmail.com>
2024-02-25 15:48:04 +0000

    edit: main.tf

--- main.tf
+++ main.tf
@@ -47,42 +47,6 @@ resource "yandex_compute_instance" "vm-1" {
 
 }
 
-#resource "yandex_compute_instance" "vm-2" {
-#  name = "tf-2"
-#  platform_id = "standard-v1"
-#  zone = "ru-central1-a"
-# 
-#  resources {
-#    cores  = 2
-#    memory = 2
-#  }
-# 
-#  boot_disk {
-#    initialize_params {
-#      image_id = "fd8bt3r9v1tq5fq7jcna"
-#    }
-#  }
-# 
-#  network_interface {
-#    subnet_id = yandex_vpc_subnet.subnet-1.id
-#    nat       = true
-#  }
-# 
-#}
-
-
-
-#resource "yandex_vpc_network" "network-1" {
-#  name = "tr-nw"
-#}
-
-#resource "yandex_vpc_subnet" "subnet-1" {
-#  name           = "tr-nw-subnet"
-#  zone           = "ru-central1-a"
-#  network_id     = "${yandex_vpc_network.network-1.id}"
-#  v4_cidr_blocks = ["172.18.10.0/24"]
-#}
-
 output "internal_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
